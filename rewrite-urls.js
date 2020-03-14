25764ef9b3fe2ca3ea652cfa60cbea0473b1f50a
Philipp Claßen <philipp.classen@posteo.de>
2020-03-14 17:59:44 +0100

    Always assume Python 3

--- rewrite-urls.js
+++ rewrite-urls.js
@@ -1,15 +1,19 @@
 $("a").each(function() {
   const old = this.href;
   if (this.href.startsWith('https://crystal-lang.org/api/')) {
-    this.href = this.href.replace(/https:\/\/crystal-lang.org\/api\/[^\\/]+\//, 'https://crystal-lang.org/api/latest/');
+    this.href = this.href.replace(/^https:\/\/crystal-lang.org\/api\/[^\\/]+\//, 'https://crystal-lang.org/api/latest/');
     return;
   }
   if (this.href.startsWith('https://www.boost.org/doc/libs/')) {
-    this.href = this.href.replace(/https:\/\/www.boost.org\/doc\/libs\/[^\\/]+\//, 'https://www.boost.org/doc/libs/release/');
+    this.href = this.href.replace(/^https:\/\/www.boost.org\/doc\/libs\/[^\\/]+\//, 'https://www.boost.org/doc/libs/release/');
     return;
   }
   if (this.href.startsWith('https://ruby-doc.org/')) {
-    this.href = this.href.replace(/(https:\/\/ruby-doc.org\/[^\\/]+)-[^\\/]+\//, (_, prefix) => `${prefix}/`);
+    this.href = this.href.replace(/^(https:\/\/ruby-doc.org\/[^\\/]+)-[^\\/]+\//, (_, prefix) => `${prefix}/`);
+    return;
+  }
+  if (this.href.startsWith('https://docs.python.org/2/library/')) {
+    this.href = this.href.replace(/^https:\/\/docs.python.org\/2\/library\//, 'https://docs.python.org/3/library/');
     return;
   }
 });

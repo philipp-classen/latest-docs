3c45ff8ffa97c9f6a98bf9af81708d0dbe5a71cd
Philipp Claßen <philipp.classen@posteo.de>
2020-06-07 23:07:48 +0200

    Detect http links to the Boost libraries

--- rewrite-urls.js
+++ rewrite-urls.js
@@ -4,10 +4,16 @@ $("a").each(function() {
     this.href = this.href.replace(/^https:\/\/crystal-lang.org\/api\/[^\\/]+\//, 'https://crystal-lang.org/api/latest/');
     return;
   }
+
   if (this.href.startsWith('https://www.boost.org/doc/libs/')) {
     this.href = this.href.replace(/^https:\/\/www.boost.org\/doc\/libs\/[^\\/]+\//, 'https://www.boost.org/doc/libs/release/');
     return;
   }
+  if (this.href.startsWith('http://www.boost.org/doc/libs/')) {
+    this.href = this.href.replace(/^http:\/\/www.boost.org\/doc\/libs\/[^\\/]+\//, 'https://www.boost.org/doc/libs/release/');
+    return;
+  }
+
   if (this.href.startsWith('https://ruby-doc.org/')) {
     this.href = this.href.replace(/^(https:\/\/ruby-doc.org\/[^\\/]+)-[^\\/]+\//, (_, prefix) => `${prefix}/`);
     return;
